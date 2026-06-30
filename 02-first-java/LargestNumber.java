96d61982dfcb1ce2b22581289aaa55d2bb59c143
prachik <prachi.kugaonkar@skai.io>
2026-06-30 15:58:51 +0530

    Largest Number - first java

---
+++ 02-first-java/LargestNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int largest = Math.max(a, b);
+        System.out.println(largest);
+
+
+
+//        if (a > b) {
+//            System.out.println(a);
+//        } else {
+//            System.out.println(b);
+//        }
+    }
+}
