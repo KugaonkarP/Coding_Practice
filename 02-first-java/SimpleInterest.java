078780bc1a61617c93698a075e09190b84f98f97
prachik <prachi.kugaonkar@skai.io>
2026-06-30 15:20:42 +0530

    Simple Interest - first java

---
+++ 02-first-java/SimpleInterest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        float r = sc.nextFloat();
+        int t = sc.nextInt();
+        float si = (p * r * t) / 100;
+        System.out.println(si);
+    }
+}
